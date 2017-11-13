388649d068899791b0c5fb3ac38468609f378a71
FakeChrystal <richard.j.hauri@gmail.com>
2017-11-13 15:09:07 +0100

    SpookyFactor => SpookyDate Done

--- spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/SpookyFactors/SpookyDate.java
+++ spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/SpookyFactors/SpookyDate.java
@@ -4,14 +4,25 @@ package team1.red.zli.spookymeter.SpookyFactors;
  * Created by admin on 13.11.2017.
  */
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class SpookyDate implements SpookyFactor {
     @Override
     public int getSpookyFactor() {
+        int spookyFactor = 0;
         Date currentTime = Calendar.getInstance().getTime();
-        return 0;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM");
+        String date = sdf.format(currentTime);
+        String[] spookyDates = {"31.10", "11.9", "1.12", /* This is today -> */"13.11"};
+        for (String thisArray : spookyDates) {
+            if(thisArray.compareTo(date) == 0){
+                spookyFactor = 7;
+            }
+        }
+
+        return spookyFactor;
     }
 
 
