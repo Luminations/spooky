460d96c38ed29d8a1a46e76cd1807adce3d45ded
FakeChrystal <richard.j.hauri@gmail.com>
2017-11-13 13:16:29 +0100

    Created 6 Spookyfactor classes, which implement Spookyfactor Interface

---
+++ spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/SpookyFactors/Date.java
@@ -0,0 +1,17 @@
+package team1.red.zli.spookymeter.SpookyFactors;
+
+/**
+ * Created by admin on 13.11.2017.
+ */
+
+public class Date implements SpookyFactor {
+    @Override
+    public int getSpookyFactor() {
+        return 0;
+    }
+
+    @Override
+    public String name() {
+        return null;
+    }
+}
