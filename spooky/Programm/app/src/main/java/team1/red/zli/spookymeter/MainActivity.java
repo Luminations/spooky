6c78deac520a0f148e3df8c0b866aab9be099b08
lesebrille <ch.p.fraser@gmail.com>
2017-11-13 11:15:47 +0100

    added spookyfactor iterface. and new activity (Spooky detail).

--- spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/MainActivity.java
+++ spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/MainActivity.java
@@ -1,13 +1,36 @@
 package team1.red.zli.spookymeter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+import team1.red.zli.spookymeter.SpookyFactors.SpookyFactor;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<SpookyFactor> spookyFactors;
+    int spooky;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button next = (Button) findViewById(R.id.button2);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), SpookyDetail.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        spookyFactors = new ArrayList<>();
+
+
+
     }
 }
---
+++ spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/SpookyFactors/SpookyFactor.java
@@ -0,0 +1,10 @@
+package team1.red.zli.spookymeter.SpookyFactors;
+
+/**
+ * Created by admin on 13.11.2017.
+ */
+
+public interface SpookyFactor {
+    public int getSpookyFactor();
+    public String name();
+}
