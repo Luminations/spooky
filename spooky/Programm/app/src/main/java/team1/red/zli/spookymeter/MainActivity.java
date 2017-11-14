2a2fea6a4fed5064d6ec89abb1ec65a122d16aea
FakeChrystal <richard.j.hauri@gmail.com>
2017-11-14 08:53:03 +0100

    temp workind

--- spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/MainActivity.java
+++ spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(myIntent, 0);
             }
         });
-        SpookyThread spookyFactorThread = new SpookyThread(handler);
+        SpookyThread spookyFactorThread = new SpookyThread(handler, this);
         spookyFactorThread.start();
     }
 }
