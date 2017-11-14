d60489ecea67bbd04c457747b663f20ccef45aa8
FakeChrystal <richard.j.hauri@gmail.com>
2017-11-14 09:54:33 +0100

    Darkness -> done.

--- spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/SpookyFactors/Darkness.java
+++ spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/SpookyFactors/Darkness.java
@@ -1,59 +1,58 @@
 package team1.red.zli.spookymeter.SpookyFactors;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by admin on 13.11.2017.
  */
 
-public class Darkness  implements SpookyFactor {
+public class Darkness extends Activity implements SpookyFactor {
     public int value = 0;
+
     @Override
     public int getSpookyFactor(Context context) {
-//        SensorManager sensorManager
-//                = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-//        Sensor lightSensor
-//                = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-//
-//        if (lightSensor == null){
-//            value = 5;
-//        }else{
-//
-//            sensorManager.registerListener(lightSensorEventListener,
-//                    lightSensor,
-//                    SensorManager.SENSOR_DELAY_NORMAL);
-//
-//
-//        }
+
+        SensorManager sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        if (lightSensor == null) {
+            value = 5;
+        } else {
+
+            sensorManager.registerListener(lightSensorEventListener,
+                    lightSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        }
         return value;
     }
 
 
+    SensorEventListener lightSensorEventListener
+            = new SensorEventListener() {
 
-//
-//    SensorEventListener lightSensorEventListener
-//            = new SensorEventListener() {
-//
-//        @Override
-//        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//            // TODO Auto-generated method stub
-//
-//        }
-//
-//        public void onSensorChanged(SensorEvent event) {
-//            // TODO Auto-generated method stub
-//            if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
-//                float currentReading = event.values[0];
-//                value =  (int)currentReading;
-//            }
-//        }
-//    };
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+//            value = (int)(10/accuracy);
+        }
+
+        public void onSensorChanged(SensorEvent event) {
+
+            if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+                float currentReading = event.values[0];
+                value = (int) (100/currentReading);
+            }
+        }
+    };
+
     @Override
     public String name() {
         return "Darkness";
