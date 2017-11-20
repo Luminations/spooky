dd238291f1344d6e0d297db6156a94035c5480b9
Michi <michi.boetschi@hotmail.ch>
2017-11-20 11:17:45 +0100

    remade spooky details

--- spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/SpookyDetail.java
+++ spooky/Programm/app/src/main/java/team1/red/zli/spookymeter/SpookyDetail.java
@@ -22,32 +22,48 @@ public class SpookyDetail extends Activity{
             ArrayList<Integer> spookyDetails = (ArrayList<Integer>) msg.obj;
 
             //title
-            TextView titleText = findViewById(R.id.textView4);
+            TextView titleText = findViewById(R.id.textView2);
             titleText.setText("Spooky Details!");
 
             //time
-            ProgressBar timeProgress = findViewById(R.id.progressBar2);
+            ProgressBar timeProgress = findViewById(R.id.progressBar);
             timeProgress.setMax(12);
-            timeProgress.setProgress(spookyDetails.get(5));
+            timeProgress.setProgress(spookyDetails.get(4));
 
             TextView timeText = findViewById(R.id.textView3);
-            timeText.setText("Time " + spookyDetails.get(5) + "/" + "12");
+            timeText.setText("Time " + spookyDetails.get(4) + "/" + "12");
 
             //date
-            ProgressBar dateProgres = findViewById(R.id.progressBar4);
+            ProgressBar dateProgres = findViewById(R.id.progressBar2);
             dateProgres.setMax(7);
-            dateProgres.setProgress(spookyDetails.get(4));
+            dateProgres.setProgress(spookyDetails.get(3));
 
-            TextView dateText = findViewById(R.id.textView5);
-            dateText.setText("Date " + spookyDetails.get(4) + "/" + "7");
+            TextView dateText = findViewById(R.id.textView4);
+            dateText.setText("Date " + spookyDetails.get(3) + "/" + "7");
 
             //battery
-            ProgressBar batteryProgress = findViewById(R.id.progressBar);
+            ProgressBar batteryProgress = findViewById(R.id.progressBar3);
             batteryProgress.setMax(10);
             batteryProgress.setProgress(spookyDetails.get(0));
 
-            TextView batteryText = findViewById(R.id.textView2);
+            TextView batteryText = findViewById(R.id.textView5);
             batteryText.setText("Battery " + spookyDetails.get(0) + "/" + "10");
+
+            //darkness
+            ProgressBar darknessProgress = findViewById(R.id.progressBar4);
+            darknessProgress.setMax(10);
+            darknessProgress.setProgress(spookyDetails.get(1));
+
+            TextView darknessText = findViewById(R.id.textView6);
+            darknessText.setText("Darkness  " + spookyDetails.get(1) + "/" + "10");
+
+            //location
+            ProgressBar locationProgress = findViewById(R.id.progressBar5);
+            locationProgress.setMax(10);
+            locationProgress.setProgress(spookyDetails.get(2));
+
+            TextView locationText = findViewById(R.id.textView7);
+            locationText.setText("Location  " + spookyDetails.get(2) + "/" + "10");
         }
     };
 
